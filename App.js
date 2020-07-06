7158a4d6efc0addde71568031b28bbd8a54cbb9b
Prahalad Belavadi <github@belavadi.com>
2020-07-06 18:29:21 +0530

    style

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Platform } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Universal React with Expo</Text>
+
+      <TextInput placeholder="This is an input box" />
     </View>
   );
 }
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  red: {
+    color: "red",
+  },
+  textStyle: {
+    textAlign: "center",
+    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto",
+  },
+  largeText: { fontSize: 44 },
+  smallText: { fontSize: 18 },
+};
