5a789e77b73ea2048d0c00c3ae6b5887b6a503a8
Prahalad Belavadi <github@belavadi.com>
2020-07-07 06:15:50 +0530

    style and implement image background

--- App.js
+++ App.js
@@ -15,20 +15,28 @@ import SearchInput from "./components/SearchInput";
 export default function App() {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
-      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+      <ImageBackground
+        source={getImageForWeather("Clear")}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            San Francisco
+          </Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-      <SearchInput placeholder="Search any city" />
+          <SearchInput placeholder="Search any city" />
+        </View>
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
 }
 const styles = {
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#34495E",
   },
   red: {
     color: "red",
@@ -36,6 +44,7 @@ const styles = {
   textStyle: {
     textAlign: "center",
     fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto",
+    color:'white'
   },
   largeText: { fontSize: 44 },
   smallText: { fontSize: 18 },
@@ -48,5 +57,21 @@ const styles = {
     marginHorizontal: 20,
     paddingHorizontal: 10,
     alignSelf: "center",
+  },
+
+  imageContainer: {
+    flex: 1,
+  },
+  image:{
+    flex:1,
+    width:null,
+    height:null,
+    resizeMode:'cover'
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
 };
