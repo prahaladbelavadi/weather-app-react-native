7e4ef857b7e2fc76f6740997d3557930bc352876
Prahalad Belavadi <github@belavadi.com>
2020-07-07 06:02:20 +0530

    import assets and handler functions

--- App.js
+++ App.js
@@ -5,24 +5,19 @@ import {
   TextInput,
   Platform,
   KeyboardAvoidingView,
+  ImageBackground,
 } from "react-native";
+
+import getImageForWeather from "./utils/getImageForWeather";
 
 import SearchInput from "./components/SearchInput";
 
 export default function App() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <KeyboardAvoidingView style={styles.container} behavior="height">
       <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
       <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
       <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-
-      {/* <TextInput
-        autoCorrect={false}
-        placeholder="Search any city"
-        placeholderTextColor="white"
-        style={styles.textInput}
-        clearButtonMode="always"
-      /> */}
 
       <SearchInput placeholder="Search any city" />
     </KeyboardAvoidingView>
