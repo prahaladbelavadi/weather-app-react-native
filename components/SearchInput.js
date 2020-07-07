aff1a8bc08841c1c41499f5133de99f31e62af3b
Prahalad Belavadi <github@belavadi.com>
2020-07-07 07:17:59 +0530

    manage state for searchinput

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { StyleSheet, TextInput, View, Text } from "react-native";
 export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+    };
+  }
+
+  handleChangeText = (newLocation) => {
+    // this.props.location = newLocation;
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+    onSubmit(text);
+    this.setState({ text: "" });
+  };
+
   render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
     return (
       <View style={styles.container}>
         {/* <Text>kmd</Text> */}
         <TextInput
           autoCorrect={false}
+          value={text}
           placeholder={this.props.placeholder}
           placeholderTextColor="white"
           underlineColorAndroid="transparent"
           style={styles.textInput}
           clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
         />
       </View>
     );
