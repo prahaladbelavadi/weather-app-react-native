8a6db283130397af5550887e05338ca03cdae7c9
Prahalad Belavadi <github@belavadi.com>
2020-07-08 23:47:41 +0530

    broken: handle loading and async data fetching via api

--- App.js
+++ App.js
@@ -7,8 +7,11 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   ImageBackground,
+  ActivityIndicator,
+  StatusBar,
 } from "react-native";
 
+import { fetchLocationId, fetchWeather } from "./utils/getImageForWeather";
 import getImageForWeather from "./utils/getImageForWeather";
 
 import SearchInput from "./components/SearchInput";
@@ -18,40 +21,90 @@ import SearchInput from "./components/SearchInput";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "" };
+    this.state = {
+      location: false,
+      error: false,
+      location: "",
+      temperature: 0,
+      weather: "",
+    };
   }
 
-  componentDidMount(){
-    this.handleUpdateLocation('San Francisco')
-    console.log('Component has mounted')
+  componentDidMount() {
+    this.handleUpdateLocation("San Francisco");
+    console.log("Component has mounted");
   }
 
-  handleUpdateLocation = (city) => {
-    this.setState({
-      location: city,
-    });
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+
+    this.setState(
+      {
+        loading: true,
+      },
+      async () => {
+        try {
+          const locationId = await fetchLocationId(city);
+          const { location, weather, temperature } = await fetchWeather(
+            locationId
+          );
+
+          this.setState({
+            loading: false,
+            error: false,
+            location,
+            weather,
+            temperature,
+          });
+        } catch (e) {
+          this.setState({ loading: false, error: true });
+        }
+      }
+    );
   };
   render() {
-    const { location } = this.state;
-
+    const { loading, error, location, weather, temperature } = this.state;
+    // console.log(loading, error, location, weather, temperature);
     return (
-      <KeyboardAvoidingView style={styles.container} behavior="height">
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather("Clear")}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-            <Text style={[styles.smallText, styles.textStyle]}>
-              Light Cloud
-            </Text>
-            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+            <ActivityIndicator animating={loading} color="white" size="large" />
 
-            <SearchInput
-              placeholder="Search any city"
-              onSubmit={this.handleUpdateLocation}
-            />
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
